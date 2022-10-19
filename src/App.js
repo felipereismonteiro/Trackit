590f918c85cc5dcd60be188e94de8d3b3649a040
felipedosreismonteiro <felipe.dreis.monteiro@gmail.com>
2022-10-18 21:45:50 -0300

    2 telas criadas

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import GlobalStyle from "./components/assets/global.js"
 import Footer from "./components/footer";
 import MainPage from "./mainPage/mainPage";
 import SigninPage from "./signinPage/signinPage";
+import styled from "styled-components";
 
 function App() {
   return (
     <>
-      <SigninPage />
-      <GlobalStyle/>
+      <GlobalStyle />
+      <MainPage />
     </>
   )
 }
